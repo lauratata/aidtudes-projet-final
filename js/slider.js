3e4a2ab37c040b05dc64118d426b2bb725cc2111
Laura Taormina <laura.taormina@edu.univ-fcomte.fr>
2021-05-29 19:21:27 +0200

    - Slider matieres - CGU + Confidentialite HTML/CSS

--- js/slider.js
+++ js/slider.js
@@ -1,3 +1,18 @@
 document.addEventListener('DOMContentLoaded', function () {
     new Splide('.splide').mount();
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#image-slider').mount();
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    new Splide('#card-slider', {
+        perPage: 2,
+        breakpoints: {
+            600: {
+                perPage: 1,
+            }
+        }
+    }).mount();
+});
