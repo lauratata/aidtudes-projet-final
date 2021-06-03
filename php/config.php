4c3eb43a24db664f68529c846ce55f2d79f0ae3c
Laura Taormina <laura.taormina@edu.univ-fcomte.fr>
2021-06-03 17:46:26 +0200

    connexion à la base de données infomaniak

--- php/config.php
+++ php/config.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $bdd = new PDO("mysql:host=i41w1.myd.infomaniak.com;port=3306;dbname=i41w1_aidtudes;charset=utf8", 'i41w1_laurataorm', 'tRVfux_4xH5');
+    $bdd = new PDO("mysql:host=i41w1.myd.infomaniak.com;port=3306;dbname=i41w1_aidtudes", 'i41w1_laurataorm', 'tRVfux_4xH5');
 } catch (Exception $e) {
     die('Erreur' . $e->getMessage());
 }
